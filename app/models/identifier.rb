b53250560f7e6259d03cd582b1545d6e11d9e9ba
mike castleman <m@mlcastle.net>
2008-11-11 15:40:13 -0500

    show blank identifier source, but don't allow to save without one

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -4,4 +4,7 @@ class Identifier < ActiveRecord::Base
   belongs_to :identifier_source
   xml_string "identifier", :identifier
   xml_picklist "identifierSource", :identifier_source, IdentifierSource
+  
+  validates_length_of :identifier, :minimum => 1
+  validates_presence_of :identifier_source
 end
