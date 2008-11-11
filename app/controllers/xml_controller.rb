fd33e7525cbcbcfc454c2b119da8203b005e57d3
mike castleman <m@mlcastle.net>
2008-11-11 15:04:15 -0500

    robustness improvements in xml import

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -16,9 +16,13 @@ class XmlController < ApplicationController
             next if entry.name =~ /[\/\\]\.[^\/\\]*/ # and hidden files too
             begin
               a = Asset.from_xml(zip.read(nil, String.new))
-              a.destroy_existing
-              a.save
-              successes << a.titles[0].title
+              if a.valid?
+                a.destroy_existing
+                a.save
+                successes << a.titles[0].title
+              else
+                failures << "#{entry.name} is not valid"
+              end
             rescue Exception => e
               failures << "#{e.to_s} on #{entry.name}"
             end
@@ -32,9 +36,14 @@ class XmlController < ApplicationController
       else
         begin
           a = Asset.from_xml(params[:xml].read)
-          a.destroy_existing
-          a.save
-          flash.now[:message] = "thanks for #{a.title}"
+          if a.valid?
+            a.destroy_existing
+            a.save
+            flash.now[:message] = "thanks for #{a.title}"
+          else
+            flash.now[:error] = "Sorry, couldn't import that record: <ul>" + a.errors.full_messages.map{|err| "<li>#{err}</li>"}.join + "</ul>"
+            raise ActiveRecord::Rollback
+          end
         rescue Exception => e
           flash.now[:error] = e.to_s
         end
