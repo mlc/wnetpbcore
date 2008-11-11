01535ea948f525a4a46b2609fd8ead33405e9f7c
mike castleman <m@mlcastle.net>
2008-11-11 15:37:26 -0500

    search help

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function searchHelp() {
+  window.open( 
+    "/static/search_help",
+    "searchHelp",
+    "height=600,width=750/inv,channelmode=0,dependent=0," +
+    "directories=0,fullscreen=0,location=0,menubar=0," +
+    "resizable=0,scrollbars=1,status=1,toolbar=0"
+  );
+}
