944ef1215588d35865158325ee4b39968b9a389a
mike castleman <m@mlcastle.net>
2008-11-17 13:08:10 -0500

    about page should show current code revision if available

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,10 @@
 class StaticController < ApplicationController
   session :off
+
+  def about
+    filename = "#{RAILS_ROOT}/REVISION"
+    if File.exist?(filename)
+      @revision = File.read(filename).chomp
+    end
+  end
 end
