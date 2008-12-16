67698d6862e07af582668ca9fce7072965b7ae06
mike castleman <m@mlcastle.net>
2008-12-16 17:53:09 -0500

    finish showing instantiations & essence tracks

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,2 +1,17 @@
 module AssetsHelper
+  def eltshow(fld)
+    update_page do |page|
+      page.visual_effect :highlight, fld, :duration => 0.5
+      page.show "hide_#{fld}"
+      page.hide "show_#{fld}"
+    end
+  end
+
+  def elthide(fld)
+    update_page do |page|
+      page.hide fld
+      page.hide "hide_#{fld}"
+      page.show "show_#{fld}"
+    end
+  end
 end
