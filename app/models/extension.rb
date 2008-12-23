7a65e10f06e07830c9c1f64961e27a972d56dff5
mike castleman <m@mlcastle.net>
2008-12-23 14:25:30 -0500

    extension

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -4,4 +4,27 @@ class Extension < ActiveRecord::Base
 
   xml_string "extension"
   xml_string "extensionAuthorityUsed"
+
+  def extension_key
+    extension_key_value[0]
+  end
+  
+  def extension_value
+    extension_key_value[1]
+  end
+
+  # warning: unwanted results will occur if your key has a colon in it.
+  def extension_key=(key)
+    self.extension = key.nil? ? extension_value : "#{key}:#{extension_value}"
+  end
+  
+  def extension_value=(value)
+    self.extension = extension_key.nil? ? value : "#{extension_key}:#{value}"
+  end
+  
+  protected
+  def extension_key_value
+    answer = extension.split(/:/, 2)
+    answer.size == 1 ? [nil, answer[0]] : answer
+  end
 end
