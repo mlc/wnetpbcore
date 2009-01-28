811f55d2103909951bcf01b53ae39a9929f2cf24
mike castleman <m@mlcastle.net>
2009-01-27 19:10:28 -0500

    warning

--- app/controllers/extension_names_controller.rb
+++ app/controllers/extension_names_controller.rb
@@ -5,5 +5,7 @@ class ExtensionNamesController < PicklistsController
   end
 
   protected
-  def should_emit_warning; false; end
+  def should_emit_warning
+    "<strong>Note:</strong> Extension names are merely aliases for extension authority / key combinations. Modifying them only affects the display in this tool, not in the underlying PBCore records."
+  end
 end
