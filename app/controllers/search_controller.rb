495874bb5ee70fbd4426a656839a655c1ba4a405
mike castleman <m@mlcastle.net>
2009-02-10 15:58:45 -0500

    refactor guided search and support editing current query.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,14 @@
 class SearchController < ApplicationController
-  FIELDS = [:identifier, :title, :subject, :description, :genre, :relation,
-    :coverage, :audience_level, :audience_rating, :creator, :contributor,
-    :publisher, :rights, :extension, :location, :annotation, :date]
-
   def index
-    @fields = [:full_text] + FIELDS
+    @search = GuidedSearchContainer.from_string(params[:model])
   end
 
   def search
-    query = (params.has_key?(:full_text) && !params[:full_text].empty?) ? [params[:full_text]] : []
-    query += FIELDS.select{|f| params.has_key?(f) && !params[f].empty?}.map{|f| "@#{f.to_s} #{params[f]}"}
-    redirect_to :controller => 'assets', :action => 'index', :q => query.join(" ")
+    if params[:search].respond_to?(:[])
+      redirect_to :controller => 'assets', :action => 'index', :q => GuidedSearchContainer.new(params[:search]).to_s
+    else
+      redirect_to :action => 'index'
+    end
   end
 
   def authorized?(action = action_name, resource = nil)
---
+++ app/models/guided_search_container.rb
@@ -0,0 +1,44 @@
+class GuidedSearchContainer
+  FIELD_NAMES = [:identifier, :title, :subject, :description, :genre, :relation,
+    :coverage, :audience_level, :audience_rating, :creator, :contributor,
+    :publisher, :rights, :extension, :location, :annotation, :date]
+
+  attr_accessor :full_text
+
+  def initialize(params = nil)
+    @fields = HashWithIndifferentAccess.new(params.to_hash)
+    self.full_text = @fields.delete(:full_text)
+  end
+
+  def self.from_string(query_string)
+    args = query_string.split(/ *@/)
+    h = { :full_text => args.shift }
+    args.each do |arg|
+      key, value = arg.split(/ +/, 2)
+      h[key] = value
+    end
+    GuidedSearchContainer.new(h)
+  end
+
+  def to_s
+    query = [full_text] + @fields.map{|k,v| (FIELD_NAMES.include?(k.to_sym) && !v.empty?) ? "@#{k.to_s} #{v}" : nil}
+    query = query.select{|x| !(x.nil? || x.empty?)}
+    query.join(" ")
+  end
+
+  def method_missing(method, *args)
+    if FIELD_NAMES.include?(method.to_sym)
+      @fields[method]
+    elsif (method.to_s[-1] == 61 && FIELD_NAMES.include?(method.to_s[0..-2].to_sym))
+      @fields[method.to_s[0..-2]] = args[0]
+    else
+      super(method, *args)
+    end
+  end
+
+  def respond_to?(method)
+    FIELD_NAMES.include?(method.to_sym) ||
+      (method.to_s[-1] == 61 && FIELD_NAMES.include?(method.to_s[0..-2].to_sym)) ||
+      super(method)
+  end
+end
