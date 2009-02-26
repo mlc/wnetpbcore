f4089fb89f4c83e7476cf55bdfc181ef5f411f4e
mike castleman <m@mlcastle.net>
2009-02-26 01:30:11 -0500

    allow blank titletype selection

--- lib/form_extensions.rb
+++ lib/form_extensions.rb
@@ -1,6 +1,12 @@
 class ActionView::Helpers::FormBuilder
   def custom_select(method, options = {})
+    if (options.delete(:allow_blank))
+      extra = '<option value=""></option>'
+    else
+      extra = ''
+    end
     "<select name=\"#{@object_name.to_s}[#{method.to_s}]\">" +
+      extra +
       yield +
       "</select>"
   end
