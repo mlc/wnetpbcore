1dd551e6313a15650d73f01e5a1bf54d182d5b74
mike castleman <m@mlcastle.net>
2009-05-12 13:10:19 -0400

    minor optimization

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -9,7 +9,7 @@ class AssetsController < ApplicationController
     @query = params[:q]
     pageopts = {:page => params[:page] || 1, :per_page => 20}
     pageopts[:page] = 1 if pageopts[:page] == "" || pageopts[:page].to_i < 1
-    asset_includes = [:titles, {:identifiers => [:identifier_source]}, {:instantiations => [:format, :format_ids, :annotations, :borrowings]}]
+    asset_includes = [:titles, {:identifiers => [:identifier_source]}, {:instantiations => [:format, :format_ids, :annotations, :borrowings]}, :descriptions]
     @search_object = @query ? 
       AssetTerms.search(@query, {:match_mode => :extended, :include => {:asset => asset_includes}, :order => params[:bydate] ? "updated_at DESC" : nil}.merge(pageopts)) :
       Asset.paginate(:all, {:order => 'updated_at DESC', :include => asset_includes}.merge(pageopts))
