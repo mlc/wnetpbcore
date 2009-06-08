4b23412ba8a44005bc1069d9a1b8f8c212c16b72
mike castleman <m@mlcastle.net>
2009-06-08 11:55:45 -0400

    Revert "allow stages to override deploy_to variable"
    This reverts commit 56c65157be2f2e211e2e9c10e947f7ba34db7a26.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repository,  "git://git.mlcastle.net/pbcore.git"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}" unless exists?(:deploy_to)
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
