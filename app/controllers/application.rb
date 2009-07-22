bf675f3e34e0c76292e30af8917a5bb835fc57ab
mike castleman <m@mlcastle.net>
2009-07-22 17:26:31 -0400

    display login page instead of failing with a weird error message

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -11,17 +11,17 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'c862b066d8e3a1d1df3bcf1404579ba2'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  # from your application log (in this case, all fields with names like "password").
+  filter_parameter_logging :password
 
   def initialize(*args)
     super(*args)
     @alternates = []
   end
-  
+
   def alternate(type, *args_for_url)
     @alternates << [url_for(*args_for_url), type]
   end
@@ -29,5 +29,14 @@ class ApplicationController < ActionController::Base
   protected
   def set_current_user
      Authorization.current_user = current_user
+  end
+
+  def permission_denied
+    if logged_in?
+      render :text => "You are not allowed to access this action.", :status => 403
+    else
+      flash[:message] = "You must log in before proceeding."
+      redirect_to login_path
+    end
   end
 end
