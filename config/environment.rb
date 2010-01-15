f7a2c21f32263531a0d41f21f608c61e08918f5a
mike castleman <m@mlcastle.net>
2010-01-15 14:42:06 -0500

    upgrade to current thinking-sphinx
    remove old version from vendor/plugins, depend on a recent version of
    the gem, and update configuration and documentation appropriately.

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem 'uuidtools'
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'whenever', :lib => false, :source => 'http://gemcutter.org'
+  config.gem 'thinking-sphinx', :lib => "thinking_sphinx", :version => ">=1.3.14", :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
