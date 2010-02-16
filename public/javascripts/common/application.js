f73b4076184f634f9a3e7314fab156d82eea6a1c
mike castleman <m@mlcastle.net>
2010-02-15 19:19:50 -0500

    popup about page and clean up styling for same

--- public/javascripts/common/application.js
+++ public/javascripts/common/application.js
@@ -1,16 +1,6 @@
 /* make transparent PNGs work in IE6 */
 $(function(){$('body').supersleight({shim: '/imgages/x.gif'})});
 
-function searchHelp() {
-  window.open(
-    "/static/search_help",
-    "searchHelp",
-    "height=600,width=750/inv,channelmode=0,dependent=0," +
-    "directories=0,fullscreen=0,location=0,menubar=0," +
-    "resizable=0,scrollbars=1,status=1,toolbar=0"
-  );
-}
-
 function checkMerge() {
   var count = $(".mergebox input:checked").length;
   if (count >= 2)
@@ -38,24 +28,33 @@ function approveMerge() {
 }
 
 $(function() {
-   $("#super-dangerous").submit(function() {
-       var prompt = 'Are you sure you wish to permanently destroy all ' + $("#total_entries").text() + ' records ';
-       prompt += 'matching the query \u201c' + $("#destroy_found_set_q").val() + '\u201d?';
-       prompt += "\n\nThis functionality is EXTREMELY dangerous, and there is NO undo available.";
-       if (confirm(prompt)) {
-           $("#super-dangerous input[type='submit']").attr("value", "Please wait\u2026").attr("disabled", true);
-           return true;
-       } else {
-           return false;
-       }
-   });
-   /* default to disabling the button, so it can not be clicked without JS */
-   $("#super-dangerous input:disabled").removeAttr("disabled");
-});
+    $("#super-dangerous").submit(function() {
+	var prompt = 'Are you sure you wish to permanently destroy all ' + $("#total_entries").text() + ' records ';
+	prompt += 'matching the query \u201c' + $("#destroy_found_set_q").val() + '\u201d?';
+	prompt += "\n\nThis functionality is EXTREMELY dangerous, and there is NO undo available.";
+	if (confirm(prompt)) {
+            $("#super-dangerous input[type='submit']").attr("value", "Please wait\u2026").attr("disabled", true);
+            return true;
+	} else {
+            return false;
+	}
+    });
+    /* default to disabling the button, so it can not be clicked without JS */
+    $("#super-dangerous input:disabled").removeAttr("disabled");
 
-$(function() {
     $('#thumbnail_form').submit(function() {
 	$("input[type='submit'])").attr({"disabled": true, "value": "Uploading\u2026"});
 	return true;
+    });
+
+    $('a.popmeup').click(function() {
+	window.open(
+	    $(this).attr("href"),
+	    "pbcorePopup",
+	    "height=600,width=700/inv,channelmode=0,dependent=0," +
+		"directories=0,fullscreen=0,location=0,menubar=0," +
+		"resizable=0,scrollbars=1,status=1,toolbar=0"
+	);
+	return false;
     });
 });
