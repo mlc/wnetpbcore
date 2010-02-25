6db402d0bfe5ef50dc6a74f5bae0d2251be86ee7
mike castleman <m@mlcastle.net>
2010-02-25 17:05:32 -0500

    use :remote_cache cap deploy strategy. dunno why we weren't already.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :repository,  "git://git.mlcastle.net/pbcore.git"
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :remote_cache
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
