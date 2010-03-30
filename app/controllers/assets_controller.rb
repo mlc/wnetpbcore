0ab74399778416ff9c1754fa652604f00cbbd450
mike castleman <m@mlcastle.net>
2010-03-30 14:35:05 -0400

    avoid throwing stupid exceptions under stupid circumstances

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -26,6 +26,8 @@ class AssetsController < ApplicationController
       Asset::FACET_NAMES.each do |facet_name|
         facet facet_name, :limit => 15
         if the_params["facet_#{facet_name}"]
+          the_params["facet_#{facet_name}"].reject!{|val| val.empty?}
+
           the_params["facet_#{facet_name}"].each do |value|
             with facet_name, value
           end
