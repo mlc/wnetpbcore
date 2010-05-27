36a54c5409e67d599f0ed2fab7a45c678cfaea3b
mike castleman <m@mlcastle.net>
2010-05-27 13:06:21 -0400

    migrations are fun, old ones even more so.

--- db/migrate/20081014191043_add_delta_index_column.rb
+++
@@ -1,9 +0,0 @@
-class AddDeltaIndexColumn < ActiveRecord::Migration
-  def self.up
-    add_column :assets, :delta, :boolean, :null => false
-  end
-
-  def self.down
-    remove_column :assets, :delta
-  end
-end
