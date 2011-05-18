866c43fe9537cedcaf625a00b0360c15ac49cd1f
mike castleman <m@mlcastle.net>
2011-05-17 22:00:48 -0400

    support for more than request_email recipient

--- lib/app_config.rb
+++ lib/app_config.rb
@@ -7,6 +7,7 @@ module PBCore
     begin
       json_config = ActiveSupport::JSON.decode(File.read(File.join(RAILS_ROOT, "config", "application.json")))
       @config = json_config.with_indifferent_access
+      @config['request_email'] = @config['request_email'].join("%2c") if @config['request_email'] && @config['request_email'].respond_to(:join)
     rescue
       @config = HashWithIndifferentAccess.new
     end
