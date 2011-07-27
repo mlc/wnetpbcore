cacd28dc0035313af65e953800886058ceaa267c
Jeremy Seitz <jeremy@somebox.com>
2011-07-27 17:10:57 +0200

    check for config/authorization_rules.rb in case lazy programmers dont read README files

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,10 @@ RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+if !File.exists?(File.join(Rails.root, 'config','authorization_rules.rb'))
+  raise "Error: config/authorization_rules.rb must be present. Pick one from config/auth_rules."
+end
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
