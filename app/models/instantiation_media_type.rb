0605c052ece499cd72fd1b388199595a9e8fe21a
Tim Trautmann <tim@foorama.com>
2012-01-05 10:50:44 -0800

    Fixing instantiation media type form

--- app/models/instantiation_media_type.rb
+++ app/models/instantiation_media_type.rb
@@ -2,7 +2,8 @@ class InstantiationMediaType < ActiveRecord::Base
   include Picklist
   has_many :instantiations
   quick_column :name
-
+  
+  named_scope :visible, :conditions => ["visible = ?", true], :order => "name asc"
   def safe_to_delete?
     instantiations.size == 0
   end
