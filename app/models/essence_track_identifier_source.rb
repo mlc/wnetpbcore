1c121e0e6c26bfb57eb1631f3815a22bcf094ff8
aviflombaum <avi@flombaum.com>
2012-04-12 15:07:19 -0400

    Allow EssenceTrackIdentifierSource to be deleted

--- app/models/essence_track_identifier_source.rb
+++ app/models/essence_track_identifier_source.rb
@@ -4,6 +4,11 @@ class EssenceTrackIdentifierSource < ActiveRecord::Base
   quick_column :name
   
   def safe_to_delete?
-    essence_tracks.size == 0
+    # This throws an AR exception; Essence Tracks do not belong
+    # to Essence Tracker Identifier Sources.
+    # This is preventing deleting EssenceTrackIdentifierSources
+    # so going to default to true.
+    # essence_tracks.size == 0
+    true
   end
 end
